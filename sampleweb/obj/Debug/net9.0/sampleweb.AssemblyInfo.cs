80a0a380251d3e961e11f714a08be82950ad712b
simeptk <simeptk@gmail.com>
2025-04-21 14:43:10 +0000

    apr21-005

--- sampleweb/obj/Debug/net9.0/sampleweb.AssemblyInfo.cs
+++ sampleweb/obj/Debug/net9.0/sampleweb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sampleweb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+682e29748bca94cdab5626918ba2baf9ff3f9507")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee365cb96ee44bb6bbf41215f58f75354509743b")]
 [assembly: System.Reflection.AssemblyProductAttribute("sampleweb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sampleweb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
